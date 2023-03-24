60150505b75178bbcffa0b5004bb308ec63e0653
vitrio wibisiono <vitrio@noxus.co.id>
2023-03-24 10:51:19 +0700

    Removed incompatible lua lsp config

--- nvim/lua/akito/core/options.lua
+++ nvim/lua/akito/core/options.lua
@@ -55,11 +55,11 @@ opt.splitbelow = true
 opt.iskeyword:append("-")
 
 opt.list = true
-opt.listchars:append "space:⋅"
-opt.listchars:append "eol:↴"
+opt.listchars:append("space:⋅")
+opt.listchars:append("eol:↴")
 
-require("indent_blankline").setup {
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
-}
+require("indent_blankline").setup({
+	space_char_blankline = " ",
+	show_current_context = true,
+	show_current_context_start = true,
+})
