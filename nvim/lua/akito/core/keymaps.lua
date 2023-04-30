708b59daefdd3a825b351ca6741e5218d6ab2ada
Vitrio Wibisono <vitrio@noxus.co.id>
2023-04-30 11:14:04 +0700

    Added new nvim config

---
+++ nvim/lua/akito/core/keymaps.lua
@@ -0,0 +1,180 @@
+-- set leader key to space
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+---------------------
+-- General Keymaps
+---------------------
+
+-- explorer
+-- keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- use jk to exit inse)t mode
+keymap.set("i", "jk", "<ESC>")
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- delete single character without copying into register
+keymap.set("n", "x", '"_x')
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>") -- increment
+keymap.set("n", "<leader>-", "<C-x>") -- decrement
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+
+----------------------
+-- Plugin Keybinds
+----------------------
+
+-- neo-tree
+keymap.set("n", "<leader>e", ":NeoTreeShowToggle<CR>") -- toggle file explorer
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+
+-- telescope git commands (not on youtube nvim video)
+keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
+keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+
+-- restart lsp server (not on youtube nvim video)
+keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
+
+-- local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+keymap.set("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
+keymap.set("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
+-- Re-order to previous/next
+keymap.set("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
+keymap.set("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
+-- Goto buffer in position...
+keymap.set("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
+keymap.set("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
+keymap.set("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
+keymap.set("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
+keymap.set("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
+keymap.set("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
+keymap.set("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
+keymap.set("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
+keymap.set("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
+keymap.set("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
+-- Pin/unpin buffer
+keymap.set("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
+-- Close buffer
+keymap.set("n", "<A-c>", "<Cmd>BufferClose<CR>", opts)
+-- Wipeout buffer
+--                 :BufferWipeout
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+-- Magic buffer-picking mode
+keymap.set("n", "<C-p>", "<Cmd>BufferPick<CR>", opts)
+-- Sort automatically by...
+keymap.set("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", opts)
+keymap.set("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>", opts)
+keymap.set("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", opts)
+keymap.set("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", opts)
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
+--
+-- LSP Saga
+-- local keymap = vim.keymap.set
+-- Lsp finder find the symbol definition implement reference
+-- if there is no implement it will hide
+-- when you use action in finder like open vsplit then you can
+-- use <C-t> to jump back
+keymap.set("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
+
+-- Code action
+keymap.set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+
+-- Rename
+keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>")
+
+-- Rename word in whole project
+keymap.set("n", "gr", "<cmd>Lspsaga rename ++project<CR>")
+
+-- Peek Definition
+-- you can edit the definition file in this float window
+-- also support open/vsplit/etc operation check definition_action_keys
+-- support tagstack C-t jump back
+keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>")
+
+-- Go to Definition
+keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+
+-- Show line diagnostics you can pass argument ++unfocus to make
+-- show_line_diagnostics float window unfocus
+keymap.set("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
+
+-- Show cursor diagnostic
+-- also like show_line_diagnostics  support pass ++unfocus
+keymap.set("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
+
+-- Show buffer diagnostic
+keymap.set("n", "<leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
+
+-- Diagnostic jump can use `<c-o>` to jump back
+keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+
+-- Diagnostic jump with filter like Only jump to error
+keymap.set("n", "[E", function()
+	require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end)
+keymap.set("n", "]E", function()
+	require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+end)
+
+-- Toggle Outline
+keymap.set("n", "<leader>o", "<cmd>Lspsaga outline<CR>")
+
+-- Hover Doc
+-- if there has no hover will have a notify no information available
+-- to disable it just Lspsaga hover_doc ++quiet
+-- press twice it will jump into hover window
+keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+-- if you want keep hover window in right top you can use ++keep arg
+-- notice if you use hover with ++keep you press this keymap it will
+-- close the hover window .if you want jump to hover window must use
+-- wincmd command <C-w>w
+keymap.set("n", "K", "<cmd>Lspsaga hover_doc ++keep<CR>")
+
+-- Callhierarchy
+keymap.set("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+keymap.set("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
+
+-- Float terminal
+keymap.set({ "n", "t" }, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
+
+
+-- nvim tmux
+keymap.set('n', "<C-h>", "<cmd>NvimTmuxNavigateLeft<CR>")
+keymap.set('n', "<C-j>", "<cmd>NvimTmuxNavigateDown<CR>")
+keymap.set('n', "<C-k>", "<cmd>NvimTmuxNavigateUp<CR>")
+keymap.set('n', "<C-l>", "<cmd>NvimTmuxNavigateRight<CR>")
+-- keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+-- keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
---
+++ nvim/lua/akito/setup.lua
@@ -0,0 +1,121 @@
+-- auto install packer if not installed
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[ 
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- import packer safely
+local status, packer = pcall(require, "packer")
+if not status then
+	return
+end
+
+-- plugins installation
+return require('packer').startup(function(use)
+  -- Neovim package management
+  use "wbthomason/packer.nvim"
+
+  -- Theme
+  use "catppuccin/nvim"
+
+  -- Syntax highlighting treesitter
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = function()
+    local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+    ts_update()
+    end,
+  })
+  
+  -- Some globally dependencies
+  use "nvim-lua/plenary.nvim"
+  use "nvim-tree/nvim-web-devicons"
+
+  -- Fuzzy finding telescope
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
+
+  -- File explorer neotree
+  use({
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = { 
+      "MunifTanjim/nui.nvim",
+    }
+  })
+
+  -- Window pickers
+  use({
+    's1n7ax/nvim-window-picker',
+    tag = 'v1.*',
+    config = function()
+        require'window-picker'.setup()
+    end,
+  })
+
+  -- Tmux navigators
+  use("alexghergh/nvim-tmux-navigation")
+
+  -- Language servers and configurations
+  use("williamboman/mason.nvim")
+  use("williamboman/mason-lspconfig.nvim")
+  use("neovim/nvim-lspconfig")
+  use({
+    "glepnir/lspsaga.nvim",
+    opt = true,
+    branch = "main",
+    event = "LspAttach",
+    requires = {
+        {"nvim-tree/nvim-web-devicons"},
+        --Please make sure you install markdown and markdown_inline parser
+        {"nvim-treesitter/nvim-treesitter"}
+    }
+  })
+
+  use("jose-elias-alvarez/null-ls.nvim")
+  use("jayp0521/mason-null-ls.nvim")
+
+  -- Autocompletion
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+  use("hrsh7th/cmp-cmdline")
+  use("hrsh7th/nvim-cmp")
+
+  -- Snippets
+  use("L3MON4D3/LuaSnip")
+  use("saadparwaiz1/cmp_luasnip")
+  use("rafamadriz/friendly-snippets")
+
+  -- Git for add, remove, and changed lines
+  use "lewis6991/gitsigns.nvim"
+  use "tpope/vim-fugitive"
+
+  -- Status line lualine
+  use "nvim-lualine/lualine.nvim"
+
+  -- Tab or buffer
+  use "romgrk/barbar.nvim"
+
+  -- Auto closing parenthesis, brackets, tags, etc.
+  use("windwp/nvim-autopairs")
+  use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" })
+
+end
+)
