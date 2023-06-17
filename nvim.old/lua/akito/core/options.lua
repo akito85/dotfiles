a7b68d43a8c5e2c6fe5c8187e931c0337e70327f
Vitrio <vitrio@noxus.co.id>
2023-06-17 08:25:41 +0700

    Added more options for windows clipboard

--- nvim.old/lua/akito/core/options.lua
+++ nvim.old/lua/akito/core/options.lua
@@ -47,6 +47,20 @@ opt.updatetime = 50
 
 -- clipboard
 opt.clipboard:append("unnamedplus")
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
 
 -- split windows
 opt.splitright = true
