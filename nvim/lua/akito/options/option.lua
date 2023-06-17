28e0ac36f0853420296e8c91f9fb14279f141311
vitrio <vitrio@noxus.co.id>
2023-06-17 19:01:32 +0700

    Removed blank lines config

--- nvim/lua/akito/options/option.lua
+++ nvim/lua/akito/options/option.lua
@@ -76,8 +76,8 @@ opt.list = true
 opt.listchars:append("space:⋅")
 opt.listchars:append("eol:↴")
 
-require("indent_blankline").setup({
-	space_char_blankline = " ",
-	show_current_context = true,
-	show_current_context_start = true,
-})
+--require("indent_blankline").setup({
+--	space_char_blankline = " ",
+--	show_current_context = true,
+--	show_current_context_start = true,
+--})
