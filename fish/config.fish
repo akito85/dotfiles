e8710a37726047591a51659d3767c6f971c2364f
Vitrio Wibisono <vitrio@noxus.co.id>
2025-02-17 12:53:12 +0700

    Addes support for x-cmd

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,4 @@
+test ! -e "$HOME/.x-cmd.root/local/data/fish/rc.fish" || source "$HOME/.x-cmd.root/local/data/fish/rc.fish" # boot up x-cmd.
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
