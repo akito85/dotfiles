f3ca6e090e1f75d12e44b2bbf76cf88300ea2cdf
Vitrio Wibisono <vitrio@noxus.co.id>
2025-04-30 15:32:31 +0700

    Disabled buggy cmp-cmdline

--- nvim-minimal/lua/setup.lua
+++ nvim-minimal/lua/setup.lua
@@ -377,7 +377,7 @@ require("lazy").setup({
     dependencies = {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
+      -- "hrsh7th/cmp-cmdline",
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
     },
